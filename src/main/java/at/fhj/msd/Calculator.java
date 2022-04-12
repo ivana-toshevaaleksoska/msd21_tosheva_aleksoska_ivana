dbacde7158409fa0259a02ef4de0f4c106957642
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2022-04-12 21:53:19 +0200

    java project commit

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -1,2 +1,20 @@
-package at.fhj.msd;public class Calculator {
+package at.fhj.msd;
+
+public class Calculator {
+    public static double add (double number1, double number2){
+        return number1+number2;
+    }
+    public static double minus (double number1, double number2){
+        return number1-number2;
+    }
+    public static double divide (double number1, double number2){
+        double quotient = number1 / number2;
+        if (Double.isNaN(quotient) || Double.isInfinite(quotient))
+            throw new ArithmeticException("Division " + number1 + " / " + number2 + " isn't possible");
+
+        return number1/number2;
+    }
+    public static double multiply (double number1, double number2){
+        return number1*number2;
+    }
 }
