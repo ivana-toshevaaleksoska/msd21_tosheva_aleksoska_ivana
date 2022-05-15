4ce0c2cc620e1a8d3ff28dc2806984f64e505ece
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2022-05-15 20:08:19 +0200

    ex4 first part

--- src/test/java/at/fhj/msd/CalculatorTest.java
+++ src/test/java/at/fhj/msd/CalculatorTest.java
@@ -1,2 +1,54 @@
-package at.fhj.msd;public class CalculatorTest {
+package at.fhj.msd;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+    private Calculator calcTest;
+
+    @BeforeEach
+    public void setUp(){
+
+        calcTest = new Calculator();
+    }
+    @Test
+    public void testAdd(){
+        double a = 20;
+        double b = 10;
+        double expectedResult = 30;
+        double result = calcTest.add(a,b);
+
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void testMinus(){
+        double a = 20;
+        double b = 10;
+        double expectedResult = 10;
+        double result = calcTest.minus(a,b);
+
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void testMultiply(){
+        double a = 20;
+        double b = 10;
+        double expectedResult = 200;
+        double result = calcTest.multiply(a,b);
+
+        Assertions.assertEquals(expectedResult,result);
+    }
+
+    @Test
+    public void testDivide(){
+        double a = 20;
+        double b = 10;
+        double expectedResult = 2;
+        double result = calcTest.divide(a,b);
+
+        Assertions.assertEquals(expectedResult,result);
+    }
 }
