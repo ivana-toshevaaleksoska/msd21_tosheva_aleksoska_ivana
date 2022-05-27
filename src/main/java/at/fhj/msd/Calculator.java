0b6b6769f95637117cf7e83c84a37d1a73ba7d0c
Ivana Tosheva Aleksoska <ivana.toshevaaleksoska@edu.fh-joanneum.at>
2022-05-27 20:00:26 +0200

    Create Javadoc comments in Calculator.java class
    Add Javadoc comments to the Calculator class with annotation @author,
    @param, @return and @throws correspondingly to show who wrote the code,
    what parameters are being used in the method, what will be returned and
    the trows statement by the division method.

--- src/main/java/at/fhj/msd/Calculator.java
+++ src/main/java/at/fhj/msd/Calculator.java
@@ -3,16 +3,39 @@
 package at.fhj.msd;
 
 public class Calculator {
+
+    /**
+     * @author Ivana Tosheva Aleksoska
+     * @param number1 the first number in the addition (first addend)
+     * @param number2 the second number in the addition (second addend)
+     * @return the result of the addition (sum)
+     *
+     */
     public static double add(double number1, double number2) {
         Main.logger.debug("Parameters: " + number1 + " , "+ number2);
         return number1 + number2;
     }
 
+    /**
+     *
+     * @author Ivana Tosheva Aleksoska
+     * @param number1 the first number in the subtraction (minuend)
+     * @param number2 the second number in the subtraction (subtrahend)
+     * @return the result of the subtraction (difference)
+     */
     public static double minus(double number1, double number2) {
         Main.logger.debug("Parameters: " + number1 + " , " + number2);
         return number1 - number2;
     }
 
+    /**
+     *
+     * @author Ivana Tosheva Aleksoska
+     * @param number1 the first number in the division (dividend)
+     * @param number2 the second number in the division (divisor)
+     * @return the result of the division (quotient)
+     * @throws ArithmeticException that is caught when division with 0 happens
+     */
     public static double divide(double number1, double number2) {
         Main.logger.debug("Parameters: " + number1 + " , " + number2);
 
@@ -28,11 +51,24 @@ public class Calculator {
         return number1 / number2;
     }
 
+    /**
+     *
+     * @author Ivana Tosheva Aleksoska
+     * @param number1 the first number in the multiplication (multiplicand)
+     * @param number2 the second number in the multiplication (multiplier)
+     * @return the result of the multiplication (product)
+     */
     public static double multiply(double number1, double number2) {
         Main.logger.debug("Parameters: " + number1 + " , " + number2);
         return number1 * number2;
     }
 
+    /**
+     *
+     * @author Ivana Tosheva Aleksoska
+     * @param n the number of which we are finding the factorial
+     * @return the result of the factorial calculation
+     */
     public long factorialRec(int n) {
         if (n <= 2) {
             return n;
